7c153b7f27833acb1f8b74a8b305a4aa874c37cf
naon <naon.dude@gmail.com>
2026-03-29 10:53:45 +0000

    feat: 商品登録APIを追加

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './items.model';
 
 @Injectable()
 export class ItemsService {
   
+  private items: Item[] = []
+
   findAll() {
     return 'This is ItemsService find all.';
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
